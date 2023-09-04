acf8a104a36da5d6da2029c4b66d256946248e89
Claudron <claudiowudang@gmail.com>
2023-09-04 21:42:00 +0200

    Shipment 3: GCS

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-function getProxyConfig() {
-  if (process.env.NODE_ENV === 'production') {
-    return {
-      '/media': {
-        target: 'https://storage.googleapis.com/your-bucket-name',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/media/, '')
-      }
-    };
-  }
-  return {};
-}
-
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000,
-    strictPort: true,
-    proxy: getProxyConfig()
+    strictPort: true
   },
 });
