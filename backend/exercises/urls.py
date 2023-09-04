341267ca5950118aab6322b70435139cba86f51b
Claudron <claudiowudang@gmail.com>
2023-09-04 12:57:44 +0200

    separated frontend

--- backend/exercises/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ExerciseMethodList, ExerciseList, UserExercisesPlaylistView
-
-urlpatterns = [
-    path('exercises/', ExerciseList.as_view(), name='exercise-list'),
-    path('method/', ExerciseMethodList.as_view(), name='exercise-method-list'),
-    path('playlist/', UserExercisesPlaylistView.as_view(), name='user-exercises-playlist'),
-]
